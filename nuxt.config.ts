ffd129cde717bb91b88e05528d3fd06d5a7cdb8a
Andrew Chakdahah <chakdahah@gmail.com>
2026-01-19 22:12:49 +0000

    first draft

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/fonts.css', '~/assets/css/main.css'],
 
   app: {
     head: {
